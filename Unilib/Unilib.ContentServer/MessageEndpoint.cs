19d76818a56393c22144bdf0de0b75c74dea6400
Ruslan Semenov <ruslan@rsemenov.com>
2012-12-10 19:40:32 +0200

    make Content Server run

---
+++ Unilib/Unilib.ContentServer/MessageEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using NServiceBus;
+using Unilib.Common.Extensions;
+using log4net;
+
+namespace Unilib.ContentServer
+{
+    public class MessageEndpoint : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    {
+        /// <summary>
+        /// Perform initialization logic.
+        /// </summary>
+        public void Init()
+        {
+            SetLoggingLibrary.Log4Net(log4net.Config.XmlConfigurator.Configure);
+            
+            Configure.With()
+                .ForUnilib()
+                .RunCustomAction(() => Configure.Instance.Configurer.ConfigureComponent(() => LogManager.GetLogger("Loger"), DependencyLifecycle.SingleInstance))
+                .JsonSerializer();
+        }
+        
+    }
+}
