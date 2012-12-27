231672894fb7e75de48399ffce16767731097bec
Admin <maksym.shlykov@gmail.com>
2012-12-28 00:54:00 +0200

    commit2

---
+++ Unilib/Unilib.Frontend/Controllers/ContentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Unilib.Frontend.Controllers
+{
+    public class ContentController : Controller
+    {
+        //
+        // GET: /Content/
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase uploadFile)
+        {
+
+            return View();
+        }
+
+    }
+}
--- Unilib/Unilib.Frontend/Models/ContentModel.cs
+++ Unilib/Unilib.Frontend/Models/ContentModel.cs
@@ -5,11 +5,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Unilib.Frontend.Models
 {
-    public class RecordContentModel
+    public class ContentModel
     {
-        
+        [DisplayName("Тип файлу")]
+        public string[] dataType { get; set; }
+        public int[] SelectedThemes { get; set; }
     }
 }
